f8e114e13f757fcdff1ebf772493f1654735874b
eduardovaillant <eduardo.bvaillant@gmail.com>
2020-10-06 14:02:50 -0300

    failures and exceptions

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,5 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   Failure([List properties = const <dynamic>[]]) : super(properties);
 }
+
+// General failures
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
