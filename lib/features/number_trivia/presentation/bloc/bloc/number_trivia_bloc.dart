1ec1cacb83618d9e34ff43be0279c09ce706e128
eduardovaillant <eduardo.bvaillant@gmail.com>
2020-10-11 20:56:12 -0300

    bloc scaffolding & input conversion

---
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(Empty());
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
