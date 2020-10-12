ac0048d42de94eda9231c0611a034bbeda9f2178
eduardovaillant <eduardo.bvaillant@gmail.com>
2020-10-12 10:03:29 -0300

    bloc implementation pt1

--- lib/features/number_trivia/presentation/bloc/bloc/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_event.dart
@@ -2,13 +2,21 @@ part of 'number_trivia_bloc.dart';
 
 @immutable
 abstract class NumberTriviaEvent extends Equatable {
-  NumberTriviaEvent([List props = const <dynamic>[]]) : super(props);
+  // NumberTriviaEvent([List props = const <dynamic>[]]) : super(props);
+  @override
+  List<Object> get props => [];
 }
 
 class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  // final String numberString;
+
+  // GetTriviaForConcreteNumber(this.numberString) : super([numberString]);
   final String numberString;
 
-  GetTriviaForConcreteNumber(this.numberString) : super([numberString]);
+  GetTriviaForConcreteNumber(this.numberString);
+
+  @override
+  List<Object> get props => [numberString];
 }
 
 class GetTriviaForRandomNumber extends NumberTriviaEvent {}
